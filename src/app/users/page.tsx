f8b0a53b3a891aeb12175ca5ed67817ad9de2c8a
Kailany <kailanyiaramenco@gmail.com>
2024-03-19 14:43:56 -0300

    add shadcn/ui

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function PoweradePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Página powerade</p>
+      <Button className="bg-red-500 rounded-xl">Click me</Button> 
     </main>
   );
 }
